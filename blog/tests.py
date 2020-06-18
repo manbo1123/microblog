58531566e2ed6082053f7e9cca063ebf53aeb1da
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-06-18 12:56:16 +0900

    Create_test_of_top_page_access

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+# トップページへのアクセス
+class BlogTestCase(TestCase):
+  def setUp(self):
+    self.c = Client()
 
-# Create your tests here.
+  def test_index_access(self):
+    res = self.c.get('/')
+    # status code => 200（成功）、404(見つからない場合)、302(リダイレクト)
+    self.assertEqual(200, res.status_code)
+
+  def test_fail_access(self):
+    res = self.c.get('/unknown')
+    self.assertEqual(404, res.status_code)
