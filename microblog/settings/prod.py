0b7ccb6475c3f737ee309923daf8eef790ca2440
root <root@localhost.localdomain>
2020-06-29 01:52:50 +0900

    allowed_host modified

--- microblog/settings/prod.py
+++ microblog/settings/prod.py
@@ -3,6 +3,8 @@ from .common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ['*', ]
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
