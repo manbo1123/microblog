0ec1b97b1c4669bcf5b9c111682978cbaf35bb26
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-07-04 16:28:33 +0900

    edit_heroku_configuration

--- microblog/settings/heroku.py
+++ microblog/settings/heroku.py
@@ -1,16 +1,22 @@
 from .common import *
 
+import dj_database_url
+
+if os.environ.get('debug', False):
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+    DEBUG = True
+else:
+    DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
-    }
-}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 INSTALLED_APPS += (
     'gunicorn',
